9916b53c004eb93305961b6426daba4dd60ab4d7
André Luiz Nunes Goulart <andreluizalemao@gmail.com>
2021-03-28 20:32:23 -0300

    deploy

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://ateliware-dev-test-backend.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
